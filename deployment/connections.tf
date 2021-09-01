c370b8c3b08861e7756600095040fb8b2d0a3422
Geoffrey Pell <Geoffrey.Pell@gft.com>
2021-09-01 17:38:24 +0100

    i

--- deployment/connections.tf
+++ deployment/connections.tf
@@ -2,5 +2,6 @@ provider "google" {
   project     = var.host_project_id
   region      = var.region
   zone        = var.zone
+  credentials = "/var/secrets/google/ec-service-account-config.json"
 }
 
