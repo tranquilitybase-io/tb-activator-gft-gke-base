3ba1cb6c54ef1c00f2e51b849ee0d29660aa1bc1
Geoffrey Pell <Geoffrey.Pell@gft.com>
2021-09-09 20:46:42 +0100

    default oauth

--- deployment/variables.tf
+++ deployment/variables.tf
@@ -174,7 +174,19 @@ variable "node_pools_oauth_scopes" {
   description = "Map of lists containing node oauth scopes by node-pool name"
 
   default = {
-    all               = ["https://www.googleapis.com/auth/cloud-platform"]
+    all               = ["https://www.googleapis.com/auth/bigquery",
+                        "https://www.googleapis.com/auth/cloud-platform",
+                        "https://www.googleapis.com/auth/cloud.useraccounts.readonly",
+                        "https://www.googleapis.com/auth/compute",
+                        "https://www.googleapis.com/auth/devstorage.read_only",
+                        "https://www.googleapis.com/auth/logging.write",
+                        "https://www.googleapis.com/auth/monitoring.write",
+                        "https://www.googleapis.com/auth/monitoring",
+                        "https://www.googleapis.com/auth/pubsub",
+                        "https://www.googleapis.com/auth/service.management.readonly",
+                        "https://www.googleapis.com/auth/servicecontrol",
+                        "https://www.googleapis.com/auth/sqlservice.admin",
+                        "https://www.googleapis.com/auth/trace.append",]
     default-node-pool = []
   }
 }
