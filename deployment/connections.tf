66d8db3417c8bd3a5940d4ff833e27a60f571386
Geoffrey Pell <Geoffrey.Pell@gft.com>
2021-09-09 12:36:39 +0100

    GKE draft

--- deployment/connections.tf
+++ deployment/connections.tf
@@ -1,7 +1,6 @@
 provider "google" {
-  project     = var.host_project_id
+  project     = var.project_id
   region      = var.region
-  zone        = var.zone
   credentials = "/var/secrets/google/ec-service-account-config.json"
 }
 
