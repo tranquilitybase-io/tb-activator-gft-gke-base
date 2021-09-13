1afefc6e7751966f630e1cde20a58a71e86eefde
Geoffrey Pell <Geoffrey.Pell@gft.com>
2021-09-13 12:41:07 +0100

    manual scopes

--- beta-private-cluster/cluster.tf
+++ beta-private-cluster/cluster.tf
@@ -416,10 +416,25 @@ resource "google_container_node_pool" "pools" {
     )
     preemptible = lookup(each.value, "preemptible", false)
 
-    oauth_scopes = concat(
-      local.node_pools_oauth_scopes["all"],
-      local.node_pools_oauth_scopes[each.value["name"]],
-    )
+//    oauth_scopes = concat(
+//      local.node_pools_oauth_scopes["all"],
+//      local.node_pools_oauth_scopes[each.value["name"]],
+//    )
+
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_write",
+      "https://www.googleapis.com/auth/monitoring.write",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/pubsub",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management",
+      "https://www.googleapis.com/auth/sqlservice.admin",
+      "https://www.googleapis.com/auth/trace.append",
+      "https://www.googleapis.com/auth/cloud_debugger"
+    ]
 
     guest_accelerator = [
       for guest_accelerator in lookup(each.value, "accelerator_count", 0) > 0 ? [{
