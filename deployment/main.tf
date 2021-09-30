0e2f71e1fff9d9c07dde0db099e0bb81fb86084f
Geoffrey Pell <Geoffrey.Pell@gft.com>
2021-09-30 20:57:32 +0100

    ssh

--- deployment/main.tf
+++ deployment/main.tf
@@ -208,6 +208,7 @@ module "config_sync" {
   location         = var.location
   cluster_endpoint = module.gke_cluster.endpoint
   create_ssh_key   = "true"
+  secret_type      = "ssh"
   sync_repo        = "git@github.com:GoogleCloudPlatform/csp-config-management.git"
   sync_branch      = "1.0.0"
   policy_dir       = "foo-corp"
